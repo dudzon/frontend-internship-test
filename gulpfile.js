2cc34f7ef46c23e7e2ba0e1a0882306e83521f27
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-01-24 10:15:00 +0100

    Changed default gulp config.Added gulp-wait because sometimes VSCode doesn't compile SCSS without this package

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,72 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const gutil = require('gulp-util');
-const connect = require('gulp-connect');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const minify = require('gulp-minify');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const gutil = require("gulp-util");
+const connect = require("gulp-connect");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const minify = require("gulp-minify");
+const autoprefixer = require("gulp-autoprefixer");
+const wait = require("gulp-wait");
 
 // scripts
-gulp.task('scripts', function() {
-  browserify({entries: ['src/js/index.js'], debug: true})
-    .transform('babelify', {presets: ['es2015'], sourceMaps: true})
+gulp.task("scripts", function() {
+  browserify({ entries: ["src/js/index.js"], debug: true })
+    .transform("babelify", { presets: ["es2015"], sourceMaps: true })
     .bundle()
-    .pipe(source('index.js'))
-    .pipe(gulp.dest('build/js'))
+    .pipe(source("index.js"))
+    .pipe(gulp.dest("build/js"))
     .pipe(connect.reload());
 });
 
 // templates
-gulp.task('assets', function() {
-  gulp.src('src/**/*.html')
-    .pipe(gulp.dest('build/'))
+gulp.task("assets", function() {
+  gulp
+    .src("src/**/*.html")
+    .pipe(gulp.dest("build/"))
     .pipe(connect.reload());
 });
 
 // sass
-gulp.task('sass', function() {
-  gulp.src('src/**/*.scss')
-      .pipe(sass())
-      .pipe(autoprefixer({
-          cascade: false
-      }))
-      .pipe(concat('styles.css'))
-      .pipe(minify())
-      .pipe(rename({
-       basename: "styles",
+gulp.task("sass", function() {
+  gulp
+    .src("src/**/*.scss")
+    .pipe(wait(500))
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(concat("styles.css"))
+    .pipe(minify())
+    .pipe(
+      rename({
+        basename: "styles",
         suffix: ".min",
         extname: ".css"
-       }))
-      .pipe(gulp.dest('build/css'));
+      })
+    )
+    .pipe(gulp.dest("build/css"));
 });
 
 // watch
-gulp.task('watch', function() {
-  gulp.watch(['src/**/*.html'], ['assets']);
-  gulp.watch(['src/**/*.js', 'src/**/*.jsx'], ['scripts']);
-  gulp.watch(['src/**/*.scss'], ['sass']);
+gulp.task("watch", function() {
+  gulp.watch(["src/**/*.html"], ["assets"]);
+  gulp.watch(["src/**/*.js", "src/**/*.jsx"], ["scripts"]);
+  gulp.watch(["src/**/*.scss"], ["sass"]);
 });
 
 // dev server
-gulp.task('serve', function() {
+gulp.task("serve", function() {
   connect.server({
-    root: 'build',
+    root: "build",
     livereload: true
   });
 });
 
-gulp.task('default', ['scripts', 'assets', 'sass', 'serve', 'watch']);
+gulp.task("default", ["scripts", "assets", "sass", "serve", "watch"]);
