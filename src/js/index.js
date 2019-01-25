1d8a14dfa9075f804114ff8b55e180a84a626ec6
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-01-25 11:35:53 +0100

    Email and password validation finished

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     hideForm = document.querySelector(".popup-form__close"),
     inputEmail = document.querySelector(".popup-form__email"),
     inputPassword = document.querySelector(".popup-form__password"),
-    form = document.querySelector(".popup-form");
+    form = document.querySelector(".popup-form"),
+    warning = document.querySelector(".popup-form__warning");
 
   /***************************************
                ACTIONS
@@ -39,34 +40,75 @@ document.addEventListener("DOMContentLoaded", () => {
   const validateEmail = () => {
     let userEmail = inputEmail.value;
     const pattern = /^[_A-Za-z0-9-]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,4})$/;
-    if (pattern.test(userEmail)) {
-    } else {
-      showWarning("It's not a valid email!");
+    if (!pattern.test(userEmail)) {
+      showWarning("It's not a valid email!", "email-error", "email");
+
+      inputEmail.style.border = "1px solid #ea6b6b";
 
       //   Reset warning after input email gets focus
-      inputEmail.addEventListener("focus", resetWarning);
+
+      inputEmail.addEventListener("focus", resetInputWarning);
+    } else {
+      // hide error warning if email passes validation
+      hideWarning(warning.querySelector(".email-error"));
     }
+
+    return true;
   };
   //   Validate Password
+  const validatePassword = () => {
+    const passwordLength = inputPassword.value.length;
+    if (passwordLength < 5) {
+      showWarning(
+        "Password must have at least five characters!",
+        "password-error",
+        "password"
+      );
+      inputPassword.style.border = "1px solid #ea6b6b";
 
-  // Show warning
-  const showWarning = string => {
-    const message = document.createRange().createContextualFragment(`
-      <div class="popup-form__warning">${string}</div>
-    `);
-    form.append(message);
-    inputEmail.style.border = "1px solid #ea6b6b";
+      //   Reset warning after input password gets focus
+      inputPassword.addEventListener("focus", resetInputWarning);
+    } else {
+      // hide error warning if password passes validation
+      hideWarning(warning.querySelector(".password-error"));
+    }
+    return true;
   };
 
-  //   Reset warning
+  // Show warning, if there is already warning displayed, don't add another one.
 
-  const resetWarning = e => {
-    const warning = document.querySelector(".popup-form__warning");
-    warning.remove();
-    inputEmail.style.border = "1px solid #ccc";
-    inputEmail.value = "";
+  const showWarning = (string, className, data) => {
+    if (!checkElem(data)) {
+      const message = document
+        .createRange()
+        .createContextualFragment(
+          `<div class=${className} data-error =${data}>${string}</div>`
+        );
+      warning.append(message);
+    }
   };
 
+  //   Reset Input Warning
+
+  function resetInputWarning() {
+    this.style.border = "1px solid #ccc";
+  }
+  //   Clear error message
+  const hideWarning = elem => {
+    if (elem) {
+      elem.remove();
+    }
+  };
+
+  //   check if error message exists,to prevent adding duplicate warnings
+
+  const checkElem = data => {
+    for (let elem of warning.children) {
+      if (elem.dataset.error === data) {
+        elem.remove();
+      }
+    }
+  };
   /***************************************
                EVENTS
   ****************************************/
@@ -74,4 +116,5 @@ document.addEventListener("DOMContentLoaded", () => {
   showForm.addEventListener("click", showPopup);
   hideForm.addEventListener("click", hidePopup);
   inputEmail.addEventListener("blur", validateEmail);
+  inputPassword.addEventListener("blur", validatePassword);
 });
